ebbb54adb3922b6ec5ba648cb038dcaae8c60ee8
Brandon U <unzueta.brandon@gmai.com>
2020-10-03 15:31:51 -0700

    adding showData to display api search results to DOM

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ async function searchSongs(term) {
   showData(data);
 }
 
+//SHOW SONGS/ARTIST IN DOM
+function showData(data) {
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map(
+          (song) => `
+       <li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+      `
+        )
+        .join("")}
+    </ul> 
+  `;
+}
+
 //EVENT LISTENERS
 form.addEventListener("submit", (e) => {
   e.preventDefault();

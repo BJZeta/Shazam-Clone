35bf85c69c4d38dfd17b0ccdd936f4f7badd9a36
Brandon U <unzueta.brandon@gmai.com>
2020-10-04 20:45:16 -0700

    creating Prev and Next buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiURL = "https:api.lyrics.ovh";
+const apiURL = "https://api.lyrics.ovh";
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const result = document.getElementById("result");
@@ -28,6 +28,35 @@ function showData(data) {
         .join("")}
     </ul> 
   `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick=getMoreSongs("${data.prev}")>Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick=getMoreSongs("${data.next}")>Next</button>`
+          : ""
+      }
+    `;
+
+    console.log(data.next);
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+//PREV / NEXT BUTTON
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+
+  console.log(url);
 }
 
 //EVENT LISTENERS
